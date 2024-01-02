078bfde286e50f98d826fce73e92b637dde4faea
natlopar <natalialopezariel@gmail.com>
2024-01-02 11:22:43 +0100

    first commit 2024

--- js/main.js
+++ js/main.js
@@ -7,18 +7,18 @@ const kittenOneDesc =
   ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
 
   const mayus1= kittenOneName.toUpperCase();
-  const kittenOne = `<li class="card">
-  <article> 
-  <img
-    class="card_img"
-    src= "${kittenOneImage}";
-    alt="gatito"
-  />
-  <h3 class="card_title"> ${mayus1} </h3>
-  <h4 class="card_race"> ${kittenOneRace} </h4>
-  <p class="card_description"> ${kittenOneDesc} </p>
-  </article>
-  </li>`;
+  // const kittenOne = `<li class="card">
+  // <article> 
+  // <img
+  //   class="card_img"
+  //   src= "${kittenOneImage}";
+  //   alt="gatito"
+  // />
+  // <h3 class="card_title"> ${mayus1} </h3>
+  // <h4 class="card_race"> ${kittenOneRace} </h4>
+  // <p class="card_description"> ${kittenOneDesc} </p>
+  // </article>
+  // </li>`;
 
 
 
@@ -29,17 +29,17 @@ const kittenOneDesc =
     'Produce fascinación y curiosidad. Exótico, raro, bello, extraño..hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
 
 const mayus2= kittenTwoName.toUpperCase();
-const kittenTwo = `<li class="card">
-<article> 
-<img
-  class="card_img"
-  src="${kittenTwoImage}"
-  alt="sphynx-cat"/>
-<h3 class="card_title">${mayus2} </h3>
-<h4 class="card_race"> ${kittenTwoRace}</h4>
-<p class="card_description"> ${kittenTwoDesc} </p>
-</article>
-</li>`;
+// const kittenTwo = `<li class="card">
+// <article> 
+// <img
+//   class="card_img"
+//   src="${kittenTwoImage}"
+//   alt="sphynx-cat"/>
+// <h3 class="card_title">${mayus2} </h3>
+// <h4 class="card_race"> ${kittenTwoRace}</h4>
+// <p class="card_description"> ${kittenTwoDesc} </p>
+// </article>
+// </li>`;
 
 
   const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
@@ -62,53 +62,107 @@ ${kittenThreeDesc}
 </p>
 </li>`;
 
-list.innerHTML = kittenOne + kittenTwo + kittenThree;
+// list.innerHTML = kittenOne + kittenTwo + kittenThree;
 
 const input_search_desc = document.querySelector('.js_in_search_desc');
 
 const descrSearchText = input_search_desc.value;
-
 // if( kittenOneDesc.includes(descrSearchText) ) {
 //   kittenOne.classList.remove('hidden');
 // } else {
 //   kittenOne.classList.add('hidden');
 // }
   
-if( kittenOneDesc.includes(descrSearchText)) {
-  list.innerHTML='';
-  list.innerHTML+= kittenOne;
-} 
-if( kittenTwoDesc.includes(descrSearchText) ) {
-  list.innerHTML='';
-  list.innerHTML+= kittenTwo;
-} 
-if( kittenThreeDesc.includes(descrSearchText) ) {
-  list.innerHTML='';
-  list.innerHTML+= kittenTwo;
-}
+// if( kittenOneDesc.includes(descrSearchText)) {
+//   list.innerHTML='';
+//   list.innerHTML+= kittenOne;
+// } 
+// if( kittenTwoDesc.includes(descrSearchText) ) {
+//   list.innerHTML='';
+//   list.innerHTML+= kittenTwo;
+// } 
+// if( kittenThreeDesc.includes(descrSearchText) ) {
+//   list.innerHTML='';
+//   list.innerHTML+= kittenTwo;
+// }
 
-const buttonAdd = document.querySelector(".js-btn-add");
 
-buttonAdd.addEventListener('click', (event)=> {
-  const inputDesc = document.querySelector('.js-input-desc');
-  const inputPhoto = document.querySelector('.js-input-photo');
-  const inputName = document.querySelector('.js-input-name');
-  const labelMessageError = document.querySelector('.js-label-error');
-  const valueDesc = inputDesc.value;
-  const valuePhoto = inputPhoto.value;
-  const valueName = inputName.value;
-  console.log(valueName);
-
-  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
-    labelMessageError.innerHTML = '¡Uy! parece que has olvidado algo';
-  }
-})
 
 const buttonCancel = document.querySelector('.js-btn-cancel');
+const formSection = document.querySelector('.js-new-form');
+
 buttonCancel.addEventListener('click', (event)=> {
-  const formSection = document.querySelector('.js-new-form');
   formSection.classList.add('collapsed');
   // const resetForm = document.querySelector('.js-form-reset');
   // resetForm.innerHTML = "holahola";
   // console.log(event.currentTarget);
 })
+
+// function showNewCatForm() {
+//   newForm.classList.remove('collapsed');
+// }
+// function hideNewCatForm() {
+//   newForm.classList.add('collapsed');
+// }
+
+const linkNewFormElement= document.querySelector('.item');
+
+function showNewCatForm() {
+  formSection.classList.remove('collapsed');
+}
+function hideNewCatForm() {
+ formSection.classList.add('collapsed');
+}
+
+
+ function handleClickNewCatForm(event) {
+  event.preventDefault();
+  if (formSection.classList.contains('collapsed')) {
+    return showNewCatForm();
+  } else {
+    return  hideNewCatForm();
+  }
+}
+
+linkNewFormElement.addEventListener('click', handleClickNewCatForm);
+
+
+
+//modifica el evento para cumplir una función manejadora
+
+const buttonAdd = document.querySelector(".js-btn-add");
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const labelMessageError = document.querySelector('.js-label-error');
+const valueDesc = inputDesc.value;
+const valuePhoto = inputPhoto.value;
+const valueName = inputName.value;
+console.log(valueName);
+
+function addNewKitten(event) {
+  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
+    labelMessageError.innerHTML = '¡Uy! parece que has olvidado algo';
+  }
+}
+
+buttonAdd.addEventListener('click', addNewKitten);
+
+function renderKitten(url, desc, name, race) {
+  return `<li class="card">
+  <img
+    class="card_img"
+    src= ${url}
+    alt="maine-coon-cat"
+  />
+  <h3 class="card_title"> ${name} </h3>
+  <h4 class="card_race">${race}</h4>
+  <p class="card_description">
+  ${desc}
+  </p>
+  </li>`;
+}
+
+
+
+list.innerHTML= renderKitten(kittenOneImage,kittenOneDesc, mayus1, kittenOneRace) + renderKitten(kittenTwoImage,kittenTwoDesc, mayus2, kittenTwoRace) + renderKitten(kittenThreeImage,kittenThreeDesc, mayus3, kittenThreeRace);
